c7c46831be5b7b3d840c46ae28c30ff80c13837a
Pratiixdn <pokhrelpratik321@gmail.com>
2026-04-19 18:22:18 +0545

    add ads.txt

--- bmi_project/urls.py
+++ bmi_project/urls.py
@@ -1,7 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.http import HttpResponse
+
+def ads_txt(request):
+    content = "google.com, pub-8936104184201511, DIRECT, f08c47fec0942fa0"
+    return HttpResponse(content, content_type="text/plain")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include('allauth.urls')),
+    path('ads.txt', ads_txt),
     path('', include('bmi.urls')),
 ]
